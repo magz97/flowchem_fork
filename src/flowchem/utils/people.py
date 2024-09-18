8144d0b2a1f0127813c523a135bca8d07ead1c65
mgarcia <miguel.garcia@mpikg.mpg.de>
2024-09-18 15:24:47 +0200

    Added miguel to people.py

--- src/flowchem/utils/people.py
+++ src/flowchem/utils/people.py
@@ -1,8 +1,9 @@
 from pydantic import NameEmail
 
-__all__ = ["dario", "jakob", "wei_hsin"]
+__all__ = ["dario", "jakob", "wei_hsin", "miguel"]
 
 
 dario = NameEmail(name="Dario Cambiè", email="2422614+dcambie@users.noreply.github.com")
 jakob = NameEmail(name="Jakob Wolf", email="Jakob.Wolf@mpikg.mpg.de")
 wei_hsin = NameEmail(name="Wei-Hsin Hsu", email="Wei-hsin.Hsu@mpikg.mpg.de")
+miguel = NameEmail(name="Miguel Garcia", email="miguel.garcia@mpikg.mpg.de")
